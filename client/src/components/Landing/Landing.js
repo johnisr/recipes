1a977bb66d18f56f9397cd5390d6bb8e32c1ccb5
John <john.i.s.ramos@gmail.com>
2018-06-06 11:49:38 -0400

    Add styling to Landing page carousel

--- client/src/components/Landing/Landing.js
+++ client/src/components/Landing/Landing.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Loader } from 'semantic-ui-react';
+import { Loader, Button, Icon } from 'semantic-ui-react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import {
@@ -8,8 +8,11 @@ import {
   Slide,
   ButtonBack,
   ButtonNext,
+  Dot,
+  Image,
 } from 'pure-react-carousel';
 import 'pure-react-carousel/dist/react-carousel.es.css';
+import './Landing.css';
 
 export class Landing extends Component {
   componentDidUpdate() {
@@ -24,33 +27,62 @@ export class Landing extends Component {
       return <Loader active content="Loading" size="large" />;
     }
     return (
-      <div>
-        <div>Click Recipes to see a list of recipes</div>
-        <div>Login to add recipes</div>
-        <CarouselProvider
-          naturalSlideWidth={160}
-          naturalSlideHeight={90}
-          totalSlides={recipes.length}
-          interval={5000}
-          isPlaying
-        >
-          <Slider>
+      <CarouselProvider
+        naturalSlideWidth={150}
+        naturalSlideHeight={90}
+        totalSlides={recipes.length}
+        interval={10000}
+        isPlaying
+      >
+        <div className="slider__container">
+          <Slider className="slider">
             {recipes.sort(() => 0.5 - Math.random()).map((recipe, index) => (
               <Slide index={index} key={recipe._id}>
-                <h2>{recipe.name}</h2>
+                <h2 className="slider__title">{recipe.name}</h2>
+                <h4 className="slider__subtitle">{recipe.summary}</h4>
+
                 <Link to={`/recipes/${recipe._id}`}>
-                  <img
+                  <Image
+                    className="slider__image"
                     src={`${this.startUrl}${recipe.imageUrl[0]}`}
                     alt={recipe.name}
+                    width="100%"
                   />
                 </Link>
               </Slide>
             ))}
           </Slider>
-          <ButtonBack>Back</ButtonBack>
-          <ButtonNext>Next</ButtonNext>
-        </CarouselProvider>
-      </div>
+          <Button
+            as={ButtonBack}
+            inverted
+            icon
+            size="large"
+            className="slider__button--left"
+          >
+            <Icon name="angle left" />
+          </Button>
+          <Button
+            as={ButtonNext}
+            icon
+            inverted
+            size="large"
+            className="slider__button--right"
+          >
+            <Icon name="angle right" />
+          </Button>
+          {recipes.map((r, index) => (
+            <Button
+              as={Dot}
+              circular
+              key={`${r._id}button`}
+              size="mini"
+              compact
+              slide={index}
+              color="black"
+            />
+          ))}
+        </div>
+      </CarouselProvider>
     );
   }
 }
